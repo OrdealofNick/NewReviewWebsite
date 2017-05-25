8f56d0b47bc47398ab1c307f9fe1dd15fcfdf1c9
R. Nathan White <rnwht@posteo.net>
2017-05-25 11:50:31 -0300

    Added information to ApplicationUser, and created Favourites table.

--- NewReviewWebsite/Models/ApplicationUser.cs
+++ NewReviewWebsite/Models/ApplicationUser.cs
@@ -9,5 +9,13 @@ namespace NewReviewWebsite.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string EMail { get; set; } //PK
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+
+        //navigation properties
+        //one user may have many favourites
+        public virtual ICollection<Favourites> Favourites { get; set; }
     }
 }
---
+++ NewReviewWebsite/Models/Favourites.cs
@@ -0,0 +1,19 @@
+﻿namespace NewReviewWebsite.Models {
+    public class Favourites {
+        public string EMail { get; set; } //FK
+        public int ArtistID { get; set; } //FK
+        public string ArtistName { get; set; } //FK
+        public int AlbumID { get; set; } //FK
+        public string AlbumName { get; set; } //FK
+
+        //navigation
+        //each user may favourite many artists
+        public virtual Artist Artists { get; set; }
+       
+        //each user may favourite many genres
+        public virtual Genre Genres { get; set; }
+
+        //many users may have many favourites
+        public virtual ApplicationUser User { get; set; }
+    }
+}
